6a757b4af9fa4979f7f8474f1b0e92db1841e696
Ömer Suat Demir <108292163+omersuatdemir@users.noreply.github.com>
2022-06-28 00:11:43 +0300

    Create VeriTipleri.cs

---
+++ VeriTipleri.cs
@@ -0,0 +1,13 @@
+static void Main(string[] args)
+        {
+            int sayi1 = 3;
+            float sayi2 = 3.14f;
+            double sayi3 = 3.1d;
+            char karakter = 'A';
+            string isim = "Ömer";
+            bool deger = true;
+
+            Console.WriteLine(sayi1 + sayi2 + sayi3 + karakter + isim + deger );
+
+            Console.ReadLine();
+        }
