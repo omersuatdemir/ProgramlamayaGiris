602511992ed5a91e93c1392565a7a790a3df4039
Ömer Suat Demir <108292163+omersuatdemir@users.noreply.github.com>
2022-06-28 00:14:58 +0300

    Create TurDonusumleri.cs

---
+++ TurDonusumleri.cs
@@ -0,0 +1,15 @@
+static void Main(string[] args)
+        {
+            int sayi1 = 3;
+            float sayi2 = 3.14f;
+            double sayi3 = 3.1d;
+            char karakter = 'A';
+            string isim = "Ömer";
+            bool deger = true;
+
+            Console.WriteLine(sayi1.ToString());
+            Console.WriteLine((int)sayi2);
+            Console.WriteLine(Convert.ToString(sayi3));
+
+            Console.ReadLine();
+        }
