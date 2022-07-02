60ec6fc84ee295f33dbb9784fd45aa3a4beef9a6
Ömer Suat Demir <108292163+omersuatdemir@users.noreply.github.com>
2022-07-02 23:59:31 +0300

    Create MathLib.cs

---
+++ MathLib.cs
@@ -0,0 +1,28 @@
+static void Main(string[] args)
+        {
+            //mutlak değeri döndürür
+            Console.WriteLine(Math.Abs(-20));
+
+            //en küçüğü döndürür
+            Console.WriteLine(Math.Min(1, 2));
+
+            //en büyüğü döndürür
+            Console.WriteLine(Math.Max(1, 2));
+
+            //eşitliği kontrol eder
+            Console.WriteLine(Math.Equals(5, 5));
+
+            //üslü sayı döndürür
+            Console.WriteLine(Math.Pow(2, 3));
+
+            //en yakın tam sayıya yuvarlar
+            Console.WriteLine(Math.Round(17.65f));
+
+            //girilen sayının karekökünü döndürür
+            Console.WriteLine(Math.Sqrt(36));
+
+            //logaritmik ifade döndürür
+            Console.WriteLine(Math.Log10(100));
+
+            Console.ReadKey();
+        }
