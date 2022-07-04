ee3e4c717579f18ddfbaef21536ee9e400de1cba
Ömer Suat Demir <108292163+omersuatdemir@users.noreply.github.com>
2022-07-05 02:54:21 +0300

    Update GotoİleDöngü.cs

--- GotoİleDöngü.cs
+++ GotoİleDöngü.cs
@@ -10,6 +10,9 @@ namespace githubornek
     {
         static void Main(string[] args)
         {
+            //programlama dillerinde her şeyin mümkün olduğundan bahsetmiştik
+            //bu yapıları size giriş videolarında gösteremeyeceğim ama olduğunu görmeniz
+            //amacıyla buraya da atmak istedim. Bu sıfırdan kendimizin yapabileceği bir döngü.
             int i = 0;
             donguBasi:
             if (i <= 10)
