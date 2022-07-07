db648a1dcbe04aef4ef77f0c96fa67ea7e0face3
Ömer Suat Demir <108292163+omersuatdemir@users.noreply.github.com>
2022-07-07 14:57:52 +0300

    Create KullanıcıdanDeğerAlma.cs

---
+++ KullanıcıdanDeğerAlma.cs
@@ -0,0 +1,13 @@
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string metin = Console.ReadLine();
+            Console.WriteLine(metin);
+
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(sayi);
+
+            Console.ReadKey();
+        }
+    }
