85a98b3a17f6f797cb1db0c66952959ab0531c6e
Ömer Suat Demir <108292163+omersuatdemir@users.noreply.github.com>
2022-07-07 15:17:13 +0300

    Create İşlemÖnceliği.cs

---
+++ İşlemÖnceliği.cs
@@ -0,0 +1,10 @@
+static void Main(string[] args)
+        {
+            //örnekleri kafanızdan hesaplayarak sonuçlarını tahmin edebilirsiniz.
+            Console.WriteLine(120 / 3 - 5 * 7);
+            Console.WriteLine(165 / (31 - 26) * 3);
+            Console.WriteLine(7 * (12 + 5) - 4);
+            Console.WriteLine((85 - 45) * (13 + 17));
+
+            Console.ReadKey();
+        }
