ef11b9c46fc047ef983c3cd56d4b543fabad60b4
Ömer Suat Demir <omersuatdemir@gmail.com>
2023-03-26 13:51:52 +0300

    Create DöngüTürleri.js

---
+++ DöngüTürleri.js
@@ -0,0 +1,16 @@
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+var i = 1;
+while (i <= 10){
+    console.log(i);
+    i++;
+}
+
+var j = 1;
+do {
+    console.log(j);
+    ++j;
+}
+while (j <= 10);
