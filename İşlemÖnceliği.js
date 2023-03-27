69861982e9de3265f1d96b7dc661e868bcc9338f
Ömer Suat Demir <omersuatdemir@gmail.com>
2023-03-27 13:50:51 +0300

    Create İşlemÖnceliği.js

---
+++ İşlemÖnceliği.js
@@ -0,0 +1,5 @@
+//örnekleri kafanızdan hesaplayarak sonuçlarını tahmin edebilirsiniz.
+console.log(120 / 3 - 5 * 7);
+console.log(165 / (31 - 26) * 3);
+console.log(7 * (12 + 5) - 4);
+console.log((85 - 45) * (13 + 17));
